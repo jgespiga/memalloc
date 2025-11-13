688247d1bfe695c2c9fdbf5c6654797bedb99674
jgespiga <jgr.espiga@protonmail.com>
2025-11-13 16:21:46 +0000

    Design `MmapAllocator` Data Structures

--- src/mmap.rs
+++ src/mmap.rs
@@ -3,7 +3,99 @@ use std::{alloc::Layout, os::raw::{c_int, c_void}, ptr};
 use libc::{mmap, munmap, off_t, size_t};
 
 
-struct MmapAllocator;
+/// Virtual memory layout of a process
+/// ```text
+/// +-------------------------+
+/// |   Kernel virtual memory |  | -> invisible to the user code
+/// +-------------------------+
+/// |                         |
+/// |          Stack          |
+/// |                         |
+/// +-------------------------+
+/// |                         |
+/// |                         |
+/// |                         |
+/// |                         |
+/// +-------------------------+
+/// |                         |
+/// |          Heap           |
+/// |                         |
+/// +-------------------------+
+/// 
+/// ... Read/write and Read-only segments
+/// 
+/// ```
+
+
+
+/// [`libc::mmap`] gives as memory regions aligned with the computer
+/// page size. But, we cannot use a full Region each time
+/// user allocates memory since we will we wasting a lot of 
+/// space. Also, we cannot assume this regions are adjacent.
+/// 
+/// Therefor, we are going to use the following data structure
+/// which consists in a LinkedList of Regions which inside of them
+/// have a LinkedList of [`Block`].
+/// 
+/// ```text
+/// +-----------------------------------------------+      +-----------------------------------------------+
+/// |        | +-------+    +-------+    +-------+  |      |        | +-------+    +-------+    +-------+  |
+/// | Region | | Block | -> | Block | -> | Block |  | ---> | Region | | Block | -> | Block | -> | Block |  |
+/// |        | +-------+    +-------+    +-------+  |      |        | +-------+    +-------+    +-------+  |
+/// +-----------------------------------------------+      +-----------------------------------------------+
+/// ```
+/// 
+/// We also need to keep track of the free blocks. So now we store
+/// two separate LinkedLists. One for memory [`Region`] and the other
+/// one of free [`Block`] which you can identify by the [`Block::is_free`]
+/// flag.
+/// 
+/// So our [`MmapAllocator`] looks something like this:
+/// 
+/// ```text
+///                                           Free List
+/// 
+///                     Next free block                Next free block
+///                +---------------------+  +--------------------------------------+
+///                |                     |  |                                      |
+/// +--------------|---------------------|--|----+      +--------------------------|-------------------+
+/// |        | +---|--+    +------+    +-|--|-+  |      |        | +-------+    +--|---+    +-------+  |
+/// | Region | | Free | -> | Block | ->| Free |  | ---> | Region | | Block | -> | Free | -> | Block |  |
+/// |        | +------+    +------+    +------+  |      |        | +-------+    +------+    +-------+  |
+/// +--------------------------------------------+      +----------------------------------------------+
+/// 
+/// ```
+
+
+struct Region {
+    /// Start direction of the Region returned by [`libc::mmap`]
+    start: *mut u8,
+    /// Size of the region.
+    size: usize,
+    /// Pointer to next Region
+    next: *mut Region,
+    /// First Block in the Region
+    first: *mut Block,
+}
+
+struct Block {
+    /// Size of the block.
+    size: usize, 
+    /// Flag to tell whether the block is free or not.
+    is_free: bool,
+    /// Pointer to previous block.
+    prev: *mut Block,
+    /// Pointer to next block.
+    next: *mut Block,
+}
+
+
+struct MmapAllocator {
+    /// Linked list of allocator memory [`Region`]
+    regions: *mut Region,
+    /// Computer's page size (used for aligment?)
+    page_size: usize,
+}
 
 impl MmapAllocator {
     #[inline]
