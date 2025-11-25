931ab3bfd2fcc445628c6bc2484de5f8d01b4365
jgespiga <jgr.espiga@protonmail.com>
2025-11-25 22:33:51 +0100

    Update example to log allocations

--- examples/mmap_alloc.rs
+++ examples/mmap_alloc.rs
@@ -1,23 +1,30 @@
-use std::alloc::Layout;
+use std::{alloc::Layout, ptr::NonNull};
 use memalloc::mmap::MmapAllocator;
 
-
+fn log_alloc(addr: *mut u8, layout: Layout) {
+    println!("Requested {} bytes of memory", layout.size());
+    println!("Received this address: {addr:?}");
+}
 fn main() {
     unsafe {
         let mut allocator = MmapAllocator::new();
     
-        let layout = Layout::new::<u32>();
-        // Allocated space for unsigned 32 bit integer.
-        let block1 = allocator.alloc(layout);
-        println!("{:?}", block1);
-        let block2 = allocator.alloc(layout);
-        println!("{:?}", block2);
+        let layout1 = Layout::new::<u8>();
+        let addr1 = allocator.alloc(layout1);
+        log_alloc(addr1, layout1);
 
-        println!("Deallocating block1");
-        allocator.dealloc(block1);
-        
-        let block3 = allocator.alloc(layout);
-        println!("Should be first addr {:?}", block3);
+        let layout2 = Layout::array::<u8>(1024).unwrap();
+        let addr2 = allocator.alloc(layout2);
+        log_alloc(addr2, layout2);
+
+        let layout3 = Layout::array::<u8>(4096).unwrap();
+        let addr3 = allocator.alloc(layout3);
+        log_alloc(addr3, layout3);
+
+        println!("Deallocating everything...");
+        allocator.dealloc(addr1);
+        allocator.dealloc(addr2);
+        allocator.dealloc(addr3);
     }
 }
 
